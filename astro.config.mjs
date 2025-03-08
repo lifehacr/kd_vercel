b3a555342e81ce62eb3010000edde757192f1f1f
Michel A <lifehackr@icloud.com>
2025-03-08 23:12:06 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import netlify from "@astrojs/netlify"; // ✅ Import Netlify adapter
 import alpinejs from "@astrojs/alpinejs";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 import embeds from "astro-embed/integration";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
@@ -15,6 +15,7 @@ import remarkMath from "remark-math";
 export default defineConfig({
   site: "https://dominikhofer.me",
   trailingSlash: "never",
+  // TODO: Configure only icons in use: https://www.astroicon.dev/reference/configuration#include
   integrations: [
     embeds({
       services: {
@@ -29,8 +30,8 @@ export default defineConfig({
     sitemap(),
     icon(),
   ],
-  output: "static", // ✅ Use "static" for Netlify
-  adapter: netlify(), // ✅ Use Netlify adapter
+  output: "hybrid",
+  adapter: vercel(),
   markdown: {
     rehypePlugins: [
       [rehypeExternalLinks, { target: "_blank", rel: ["noopener"] }],
